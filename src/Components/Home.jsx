50321e376cf233d855b52c3830a739820b9ecef0
DiogoPonte <diogopontemartinho@hotmail.com>
2023-04-09 21:55:19 +0100

    moved file upload from ./Home to ./Sales, as my current expertise are not enough to pass data between components. added some info in home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,51 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { usePapaParse } from 'react-papaparse';
+import React, { useEffect } from 'react';
+
 
 
 
 
 const Home = () => {
-
-    const { readString } = usePapaParse();
-    
-    const [file, setFile] = useState();
-
-    const fileReader = new FileReader();
-
-    const handleChange = (e) => {
-        setFile(e.target.files[0]);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (file) {
-            fileReader.onload = function (event) {
-                const csvOutput = event.target.result;
-            };
-
-            fileReader.readAsText(file);
-        }
-
-        console.log(file)
-
-    };
-
-    let [csv, setParsedCsvData] = useState([]);
-    
-
-    const handleFile = () => {
-        readString(file, {
-            worker: true,
-            header: true,
-            skipEmptyLines: true,
-            complete: (results) => {
-                setParsedCsvData(results.data)
-            }
-        });
-        
-    };
-    
 
     useEffect(() => {
         document.title = "Home"
@@ -54,55 +13,18 @@ const Home = () => {
     return (  
         <div className="home">
             <h2>Homepage</h2>
-            <h3 className="action"> Please, select file to upload</h3>            
-            <form className = "csv_input" onSubmit={handleSubmit}>
-                <input type="file" accept=".csv" onChange={handleChange} />
-                 <button onClick = {() => handleFile()} type="submit" className="submit_btn">Submit</button>
-            </form>
-            <table className='table'>
-                <thead>
-                    <tr>
-                    <th>Data</th>
-                    <th>Hora</th>
-                    <th>Produto</th>
-                    <th>ISIN</th>
-                    <th>Bolsa de origem</th>
-                    <th>Bolsa</th>
-                    <th>Quantidade</th>
-                    <th>Preços</th>
-                    <th>Moeda (Preço)</th>
-                    <th>Valor Local</th>
-                    <th>Moeda (Valor Local)</th>
-                    <th>Valor</th>
-                    <th>Moeda (Valor)</th>
-                    <th>Taxa de cãmbio</th>
-                    <th>Comissões</th>
-                    <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {csv && csv.map((parsedData, index) => (
-                    <tr key={index}>
-                        <td>{parsedData.Data}</td>
-                        <td>{parsedData.Hora}</td>
-                        <td>{parsedData.Produto}</td>
-                        <td>{parsedData.ISIN}</td>
-                        <td>{parsedData["Bolsa de"]}</td>
-                        <td>{parsedData.Bolsa}</td>
-                        <td>{parsedData.Quantidade}</td>
-                        <td>{parsedData.Preços}</td>
-                        <td>{parsedData._1}</td>
-                        <td>{parsedData["Valor local"]}</td>
-                        <td>{parsedData._2}</td>
-                        <td>{parsedData.Valor}</td>
-                        <td>{parsedData._3}</td>
-                        <td>{parsedData["Taxa de Câmbio"]}</td>
-                        <td>{parsedData["Custos de transação"]}</td>
-                        <td>{parsedData.Total}</td>
-                    </tr>
-                    ))}
-                </tbody>
-                </table>
+
+            <h3 className="home-title">This is a training project to calculate tax profit/loss from DEGIRO's transaction report</h3>
+            <p>DEGIROS's transaction report only includes the 
+                raw information for the transactions for a given set of dates.</p>
+
+            <p>Therefore, in order to find the profit/loss for tax reporting, 
+                it's necessary to manually search for the buys that match the previous year's sales.</p>
+            <p>This tool aims to do that automatically, once provided with the transactions csv and the date range.</p>
+
+            <h3>To load the csv file, please go to the <a href="/sales">Sales</a> page.</h3>
+
+
         </div>
     );
 }
--- src/Components/Sales.jsx
+++ src/Components/Sales.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { usePapaParse } from 'react-papaparse';
+
 
 const Sales = () => {
 
@@ -6,9 +8,104 @@ const Sales = () => {
         document.title = "Sales"
      }, []);
 
+    const { readString } = usePapaParse();
+    const [file, setFile] = useState();
+    const fileReader = new FileReader();
+
+    const handleChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (file) {
+            fileReader.onload = function (event) {
+                const csvOutput = event.target.result;
+            };
+
+            fileReader.readAsText(file);
+        }
+
+        console.log(file)
+
+    };
+    
+
+    let [csv, setParsedCsvData] = useState([]);
+    
+
+    const handleFile = () => {
+        readString(file, {
+            worker: true,
+            header: true,
+            skipEmptyLines: true,
+            complete: (results) => {
+                setParsedCsvData(results.data)
+            }
+        });
+        
+    };
+
+
+    let sales = csv.filter((csv)=> csv["Quantidade"] < 0 )
+    
+
+
+    
     return ( 
-        <div className="home">
+        <div className="sales">
             <h2>Sales</h2>
+            <h3 className="action"> Please, select file to upload</h3>            
+            <form className = "csv_input" onSubmit={handleSubmit}>
+                <input type="file" accept=".csv" onChange={handleChange} />
+                 <button onClick = {() => handleFile()} type="submit" className="submit_btn">Submit</button>
+            </form>
+            {sales.length>0? (
+            <table className='table'>
+                <thead>
+                    <tr>
+                    <th>Data</th>
+                    <th>Hora</th>
+                    <th>Produto</th>
+                    <th>ISIN</th>
+                    <th>Bolsa de origem</th>
+                    <th>Bolsa</th>
+                    <th>Quantidade</th>
+                    <th>Preços</th>
+                    <th>Moeda (Preço)</th>
+                    <th>Valor Local</th>
+                    <th>Moeda (Valor Local)</th>
+                    <th>Valor</th>
+                    <th>Moeda (Valor)</th>
+                    <th>Taxa de cãmbio</th>
+                    <th>Comissões</th>
+                    <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {sales && sales.map((parsedData, index) => (
+                    <tr key={index}>
+                        <td>{parsedData.Data}</td>
+                        <td>{parsedData.Hora}</td>
+                        <td>{parsedData.Produto}</td>
+                        <td>{parsedData.ISIN}</td>
+                        <td>{parsedData["Bolsa de"]}</td>
+                        <td>{parsedData.Bolsa}</td>
+                        <td>{parsedData.Quantidade}</td>
+                        <td>{parsedData.Preços}</td>
+                        <td>{parsedData._1}</td>
+                        <td>{parsedData["Valor local"]}</td>
+                        <td>{parsedData._2}</td>
+                        <td>{parsedData.Valor}</td>
+                        <td>{parsedData._3}</td>
+                        <td>{parsedData["Taxa de Câmbio"]}</td>
+                        <td>{parsedData["Custos de transação"]}</td>
+                        <td>{parsedData.Total}</td>
+                    </tr>
+                    ))}
+                </tbody>
+                </table>) : null }
         </div>
      );
 }
